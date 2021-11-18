3cc94ca18f043359a66e11ea3cb85cfaaaa99ed9
Gabriel Pelouze <gabriel@pelouze.net>
2021-11-18 17:30:04 +0100

    fix tug repo pinning in release pdf action

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 export TEXMFHOME=/data/texmfhome
-tlmgr repository add ftp://tug.org/historic/systems/texlive/2019/tlnet-final
+tug_repository="ftp://tug.org/historic/systems/texlive/2019/tlnet-final"
+tlmgr repository add "$tug_repository"
+tlmgr option repository "$tug_repository"
 tlmgr init-usertree
 tlmgr --usermode install mdframed zref needspace libertine titling
 pandoc --pdf-engine=xelatex doc/doc_TP_coupole.md -o doc/doc_TP_coupole.pdf
