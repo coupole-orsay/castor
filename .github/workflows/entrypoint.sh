5a0508ad9257e97dd0f32aea69699d6f0ba6f10d
Gabriel Pelouze <gabriel@pelouze.net>
2021-11-18 17:47:57 +0100

    fix tug repo setup in release pdf action

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export TEXMFHOME=/data/texmfhome
-tug_repository="ftp://tug.org/historic/systems/texlive/2019/tlnet-final"
-tlmgr repository add "$tug_repository"
-tlmgr option repository "$tug_repository"
 tlmgr init-usertree
+tug_repository="ftp://tug.org/historic/systems/texlive/2019/tlnet-final"
+tlmgr --usermode repository add "$tug_repository"
+tlmgr --usermode option repository "$tug_repository"
 tlmgr --usermode install mdframed zref needspace libertine titling
 pandoc --pdf-engine=xelatex doc/doc_TP_coupole.md -o doc/doc_TP_coupole.pdf
