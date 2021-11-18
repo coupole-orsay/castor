ab920cee2e487f34503f136cad3b040b28ea861f
Gabriel Pelouze <gabriel@pelouze.net>
2021-11-18 17:21:56 +0100

    pin tug repo version to 2019 in release pdf action

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export TEXMFHOME=/data/texmfhome
+tlmgr repository add ftp://tug.org/historic/systems/texlive/2019/tlnet-final
 tlmgr init-usertree
 tlmgr --usermode install mdframed zref needspace libertine titling
 pandoc --pdf-engine=xelatex doc/doc_TP_coupole.md -o doc/doc_TP_coupole.pdf
